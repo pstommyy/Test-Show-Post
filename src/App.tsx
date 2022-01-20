e472f6f934aaef979226b04e53267e50e639b496
pstomit <pitsanuwat173@gmail.com>
2022-01-20 23:28:04 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './index.module.scss'
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          TEST 01 SHOW POST
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className={styles.showPostBox}>
+          
+      </div>
     </div>
   );
 }
---
+++ src/Component/NavTop/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavTop: React.FC = (): JSX.Element => {
+  return <div></div>;
+}
+
+export default NavTop;
