56b4b4a5ed0ddcbea7709bb5f63090d188675702
pstommyy <p.s.tommyy@gmail.com>
2022-01-21 16:32:22 +0700

    1 commit

--- src/Component/ShowPost/index.tsx
+++ src/Component/ShowPost/index.tsx
@@ -8,36 +8,36 @@ const ShowPost: React.FC =(): JSX.Element => {
       id: 1,
       name:'Username-01',
       topic:'Topic-01',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin a.'
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       id: 2,
       name:'Username-02',
       topic:'Topic-02',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin a.'
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin a.'
     },
     {
       id: 3,
       name:'Username-03',
       topic:'Topic-03',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin a.'
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.'
     },
   ]
 
   return <section className={styles.showPostBox}>
     <div className={styles.container}>
       {postMocData.map((data)=>(
-        <div className={styles.profileCard}>
+        <div key={data.id} className={styles.profileCard}>
           <div className={styles.leftSide}>
             <div className={styles.detailProfile}>
               <div>
-                <p className={styles.textName}>Username</p>
-                <p className={styles.textGrey}>Topic</p>
+                <p className={styles.textName}>{data.name}</p>
+                <p className={styles.textGrey}>{data.topic}</p>
               </div>
             <div className={styles.contractGroup}>
               <article className={styles.contractGroup}>
                 <p className={styles.textContent}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.Tempus imperdiet et amet est cursus mauris, neque, sollicitudin a.
+                {data.content}
                 </p>
               </article>
             </div>
