4d206d3105e7f7bf1e9b6be16bdc35b7eddb52a1
pstommyy <p.s.tommyy@gmail.com>
2022-01-23 13:14:05 +0700

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import styles from './index.module.scss'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 //component
 import NavTop from './Component/NavTop'
 import ShowPost from './Component/ShowPost'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1scpOpRpQd4DtirqfiTmX_uOSf1-MqNw",
+  authDomain: "test-show-post.firebaseapp.com",
+  projectId: "test-show-post",
+  storageBucket: "test-show-post.appspot.com",
+  messagingSenderId: "868955795898",
+  appId: "1:868955795898:web:1e346e6b02302e4e0fed83",
+  measurementId: "G-4CC7FC54L3"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App: React.FC = (): JSX.Element => {
   return (
